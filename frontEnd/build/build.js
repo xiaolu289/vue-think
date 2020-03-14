e4ef2998c06f92933ff737cd0e239dbb7e4bb20b
xiaolu289 <312351283@qq.com>
2020-03-14 17:26:16 +0800

    Add build error handler

--- frontEnd/build/build.js
+++ frontEnd/build/build.js
@@ -26,13 +26,24 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
       chunks: false,
       chunkModules: false
     }) + '\n\n')
-
+    const info = stats.toJson();
     if (stats.hasErrors()) {
       console.log(chalk.red('  Build failed with errors.\n'))
+      info.errors.forEach(function(item) {
+        console.error(item)
+      })
       process.exit(1)
     }
-
+    if (stats.hasWarnings()) {
+      info.warnings.forEach(function(item) {
+        console.warn(item)
+      })
+    }
     console.log(chalk.cyan('  Build complete.\n'))
+    console.log(stats.toString({
+      // Add console colors
+      colors: true
+    }));
     console.log(chalk.yellow(
       '  Tip: built files are meant to be served over an HTTP server.\n' +
       '  Opening index.html over file:// won\'t work.\n'
